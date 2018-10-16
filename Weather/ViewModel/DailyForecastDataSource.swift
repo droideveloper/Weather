57b8d2d198334fc025e68e0dd9527ef6d920c662
Fatih Şen <fatih@Fatih-MacBook-Pro.local>
2018-10-16 10:10:28 +0300

    update

--- Weather/ViewModel/DailyForecastDataSource.swift
+++ Weather/ViewModel/DailyForecastDataSource.swift
@@ -9,30 +9,15 @@
 import Foundation
 import UIKit
 
-class DailyForecastDataSource: NSObject, TableViewDataSource {
-	typealias D = DailyForecast
+class DailyForecastDataSource: BaseTableDataSource<DailyForecast> {
 
 	private let DAILY_FORECASST_CELL = "kDailyForecastCell"
-	
-	var dataSet: ObservableList<DailyForecast>
-	
-	init(dataSet: ObservableList<DailyForecast>) {
-		self.dataSet = dataSet
+  
+	override init(dataSet: ObservableList<DailyForecast>) {
+		super.init(dataSet: dataSet)
 	}
 	
-	func indentifierForIndexPath(_ indexPath: IndexPath) -> String {
+	override func indentifierForIndexPath(_ indexPath: IndexPath) -> String {
 		return DAILY_FORECASST_CELL
-	}
-	
-	public func numberOfSections(in tableView: UITableView) -> Int {
-		return 1
-	}
-	
-	public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return dataSet.count
-	}
-	
-	public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		return cellFor(tableView, indexPath: indexPath)
 	}
 }
