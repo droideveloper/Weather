ad14172204945954ff7efb4a14d0be358335bdf1
Fatih Şen <fatih@Fatih-MacBook-Pro.local>
2018-10-17 17:50:48 +0300

    updated some variants

--- Weather/Widget/CircleBackgroundImageView.swift
+++ Weather/Widget/CircleBackgroundImageView.swift
@@ -10,15 +10,13 @@ import Foundation
 import UIKit
 import CoreGraphics
 
+@IBDesignable
 class CircleBackgroundImageView: UIImageView {
   
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    // update layer
-    let layer = CAShapeLayer()
+  override func draw(_ rect: CGRect) {
+    super.draw(rect)
     let path = UIBezierPath(ovalIn: self.frame)
-    layer.path = path.cgPath
-    layer.fillColor = UIColor.parse(0x999999).cgColor
-    self.layer.insertSublayer(layer, at: 0)
+    UIColor.parse(0x000000).setFill()
+    path.fill()
   }
 }
