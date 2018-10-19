69953ea2819fb8349dc212ec2bf8bc8a87dfd8d5
droideveloper <fatih.sen.boun@gmail.com>
2018-10-19 08:04:07 +0300

    update changes

--- Weather/Widget/BackgroundView.swift
+++ Weather/Widget/BackgroundView.swift
@@ -12,20 +12,20 @@ import CoreGraphics
 
 @IBDesignable
 class BackgroundView: UIView {
- 
-  required init?(coder aDecoder: NSCoder) {
-   super.init(coder: aDecoder)
-   
-   let rect = CGRect(x: 0, y: 1, width: self.bounds.width, height: self.bounds.height - 2)
-   
-   let darkLayer = CAShapeLayer()
-   darkLayer.path = UIBezierPath(rect: self.bounds).cgPath
-   darkLayer.fillColor = UIColor.darkGray.cgColor
-   self.layer.addSublayer(darkLayer)
-   
-   let whiteLayer = CAShapeLayer()
-   whiteLayer.path = UIBezierPath(rect: rect).cgPath
-   whiteLayer.fillColor = UIColor.white.cgColor
-   self.layer.addSublayer(whiteLayer)
-  }
+
+	override func layoutSubviews() {
+		super.layoutSubviews()
+	
+		let rect = CGRect(x: 0, y: 1, width: self.bounds.width, height: self.bounds.height - 2)
+		
+		let darkLayer = CAShapeLayer()
+		darkLayer.path = UIBezierPath(rect: self.bounds).cgPath
+		darkLayer.fillColor = UIColor.darkGray.cgColor
+		self.layer.addSublayer(darkLayer)
+		
+		let whiteLayer = CAShapeLayer()
+		whiteLayer.path = UIBezierPath(rect: rect).cgPath
+		whiteLayer.fillColor = UIColor.white.cgColor
+		self.layer.addSublayer(whiteLayer)
+	}
 }
