e9b4b425109be0580f84b05eb619ca2eeb14fc9d
Fatih Şen <fatih@Fatih-MacBook-Pro.local>
2018-10-19 17:29:51 +0300

    updated some components

---
+++ Weather/View/StartUpController.swift
@@ -0,0 +1,21 @@
+//
+//  StartUpController.swift
+//  Weather
+//
+//  Created by Fatih Şen on 19.10.2018.
+//  Copyright © 2018 VNGRS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StartUpController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+  }
+}
